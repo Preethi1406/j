704b6451f9a2a9555af0f1eee99e5e78c63b0c81
Preethi1406 <30458059+Preethi1406@users.noreply.github.com>
2017-07-27 10:00:57 +0530

    Create nn.c

---
+++ nn.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ int n;
+ printf("Enter the Number:");
+ scanf("%d",&n);
+ if(n<0)
+   printf("It is Negative..");
+ if else(n>0) 
+   printf("It is positive..");
+ else
+  printf("It is Zero.. ");
+}
